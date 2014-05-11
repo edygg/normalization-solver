1efe29cbc813f864825845449a2bb8cf1e4a0bfa
Edilson Gonzalez <edilson.gonza@unitec.edu>
2014-05-11 15:23:35 -0600

    Adding Field Class and InvalidDataException

--- src/edu/unitec/normalization/Field.java
+++ src/edu/unitec/normalization/Field.java
@@ -1,15 +1,73 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
 package edu.unitec.normalization;
 
+import java.util.List;
 /**
  *
  * @author Edilson
  */
 public class Field {
+    private List<Character> names;
+    private boolean primaryKey;
+    
+    public Field(List<Character> names, boolean isPrimaryKey) throws InvalidDataException {
+        if (names.isEmpty()) {
+            throw new InvalidDataException("Field names are empty");
+        }
+        
+        for (int i = 0; i < names.size(); i++) {
+            if (!Character.isLetter(names.get(i))) {
+                throw new InvalidDataException("Field names only can be letters.");
+            }           
+        }
+        
+        this.names = names;
+        this.primaryKey = isPrimaryKey;
+    }
+    
+    public List<Character> getFieldNames() {
+        return this.names;
+    }
+    
+    public boolean isPrimaryKey() {
+        return this.primaryKey;
+    }
+    
+    public void setPrimaryKey(boolean val) {
+        this.primaryKey = val;
+    }
+
+    @Override
+    public String toString() {
+        String retVal = "";
+        
+        for (int i = 0; i < this.names.size(); i++) {
+            retVal += this.names.get(i);
+        }
+        
+        return retVal;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        
+        final Field other = (Field) obj;
+        
+        for (int i = 0; i < other.names.size(); i++) {
+            if (!this.names.contains(other.names.get(i))) {
+                return false;
+            }
+        }
+        
+        return true;
+    }
+    
     
 }
---
+++ src/edu/unitec/normalization/InvalidDataException.java
@@ -0,0 +1,14 @@
+
+package edu.unitec.normalization;
+
+/**
+ *
+ * @author Edilson
+ */
+public class InvalidDataException extends Exception {
+
+    public InvalidDataException(String string) {
+        super(string);
+    }
+    
+}
